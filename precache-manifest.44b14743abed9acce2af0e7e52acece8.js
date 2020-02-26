afb52b7a5d1ae1fed8057dcc2f40db409eb1552d
jakob <jakmanne@gmail.com>
2020-02-26 17:37:41 +0100

    Updates

---
+++ precache-manifest.44b14743abed9acce2af0e7e52acece8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6b714ccc3e4f8dcdf7d508d4126c90",
+    "url": "/Rutetabell/index.html"
+  },
+  {
+    "revision": "fa5eb9b25a2544b471c9",
+    "url": "/Rutetabell/static/css/main.11c30420.chunk.css"
+  },
+  {
+    "revision": "56f6bffd42f593f5dec0",
+    "url": "/Rutetabell/static/js/2.fa24fe03.chunk.js"
+  },
+  {
+    "revision": "fa5eb9b25a2544b471c9",
+    "url": "/Rutetabell/static/js/main.9af1c223.chunk.js"
+  },
+  {
+    "revision": "1cfc17ffefb317b1d185",
+    "url": "/Rutetabell/static/js/runtime~main.eeb55e4c.js"
+  }
+]);
